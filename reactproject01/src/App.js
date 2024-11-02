ad6687973dd60d307297704a839c42982d801d59
ishmeet-codes <ishmeetsinghxa@gmail.com>
2024-11-02 17:41:39 +0530

    v3

---
+++ reactproject01/src/App.js
@@ -0,0 +1,11 @@
+import Chai from "./chai"
+function App() {
+  return (
+    <>
+    <h1>Ishmeet Singh</h1>
+    <Chai/>
+    </>
+  );
+}
+
+export default App;
---
+++ reactproject01/src/chai.js
@@ -0,0 +1,6 @@
+function Chai(){
+    return(
+        <h1>Chai pila doo</h1>
+    )
+}
+export default Chai
